7149b9002c58929e6ba506a6c0297e650300998d
lilee <user.email>
2024-07-15 11:06:47 +0800

    fix: 修复样式

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -7,7 +7,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden md:block border-r bg-muted/40">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex min-h-screen flex-col gap-2">
           <div className="h-header flex items-center border-b px-4 lg:px-6">
             <Link href="/" className="flex items-center gap-2 font-semibold">
               <Notebook className="h-5 w-5" />
